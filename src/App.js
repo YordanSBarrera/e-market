d964744d42f11130194232ddac6e1580e17f6373
YordanSBarrera <yordansaldanabarrera@gmail.com>
2022-06-24 22:46:42 -0300

    error con bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import "./App.css"
+import "./App.css";
 import Navbar from "./component/navbar";
 import Home from "./component/home";
+import { Router, Route } from "react-router-dom";
+import Products from "./component/products";
 
 function App() {
   return (
     <>
       <Navbar />
-      <Home/>
+      <Router>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/products" component={Products} />
+        <Home />
+      </Router>
     </>
   );
 }
